7b63b064737bf8dec5fc2975b166575140a6104a
CHOIJEWON <alsrn6040@kakao.com>
2022-12-05 14:56:27 +0900

    test_api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getTime(): string {
+    return `${dayjs('2021-10-10 10:30:25', 'YYYY-MM-DD HH:mm:ss')}`;
   }
 }
